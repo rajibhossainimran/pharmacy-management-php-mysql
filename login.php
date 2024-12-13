0c738780b74e1e60f5de72f85452adc6796d1d4f
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-13 11:26:58 +0600

    tailwind css setup complete

--- login.php
+++ login.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./output.css" rel="stylesheet">
 </head>
 <body>
-    <h1>this is login page</h1>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </body>
 </html>
