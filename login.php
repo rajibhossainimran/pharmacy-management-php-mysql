1956cfc7d2eb3d8c7e79d718820f6d27bd7c7173
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-13 11:48:55 +0600

    tailwind connection problem solve

--- login.php
+++ login.php
@@ -3,10 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="./output.css" rel="stylesheet">
+  <link href="src/output.css" rel="stylesheet">
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline">
+  <h1 class="text-6xl font-bold underline text-red-500">
     Hello world!
   </h1>
 </body>
