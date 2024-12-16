a6104ae5d502d69b66dba61be84f0ce38d80ee18
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-17 00:56:19 +0600

    logout method added

--- logout.php
+++ logout.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>DaisyUI in PHP</title>
-  <link rel="stylesheet" href="output.css">
-</head>
-<body>
-  <div class="card w-96 bg-base-200 shadow-xl">
-    <div class="card-body">
-      <h2 class="card-title">Hello DaisyUI!</h2>
-      <button class="btn btn-primary">Click Me</button>
-    </div>
-  </div>
-</body>
-</html>
+<?php
+session_start();
+session_unset();
+session_destroy();
+header('Location: index.php');
+exit();
+?>
