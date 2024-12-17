2a0388c9e0841ff21ab7c9d0f232a0df56b01db7
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-18 00:00:23 +0600

    select admin part

--- index.php
+++ index.php
@@ -89,6 +89,7 @@
                 <path
                 d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
             </svg>
+            <!-- select admin role part start  -->
             <select name="user-roll" class="select select-bordered w-full border-2 border-teal-600 rounded-md text-teal-900 text-xl focus:outline-none focus:ring-2 focus:ring-teal-500" required>
                     <option value="admin">Admin</option>
                     <option value="manager">Manager</option>
