d9e48db7f23529dd992eebe54fd281002b6d80ae
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-21 01:02:57 +0600

    sidebar munu added

--- pages/common_pages/sidebar.php
+++ pages/common_pages/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
+<aside style="background-color: #002d2d" class="app-sidebar shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
             <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="./index.html" class="brand-link"> <!--begin::Brand Image--> <img src="../../dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span class="brand-text fw-light">AdminLTE 4</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
             <div class="sidebar-wrapper">
                 <nav class="mt-2"> <!--begin::Sidebar Menu-->
@@ -10,27 +10,196 @@
                             </a>
                            
                         </li>
-                        <li class="nav-item"> <a href="./generate/theme.html" class="nav-link"> <i class="nav-icon bi bi-palette"></i>
-                                <p>Theme Generate</p>
-                            </a> </li>
-                        <li class="nav-item"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-box-seam-fill"></i>
+                        <li class="nav-item"> <a href="#" class="nav-link"> <i class="bi bi-cart4"></i>
                                 <p>
-                                    Widgets
+                                   Purchase
                                     <i class="nav-arrow bi bi-chevron-right"></i>
                                 </p>
                             </a>
                             <ul class="nav nav-treeview">
-                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
-                                        <p>Small Box</p>
+                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Add Purchase</p>
                                     </a> </li>
-                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
-                                        <p>info Box</p>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Purchase List</p>
                                     </a> </li>
-                                <li class="nav-item"> <a href="./widgets/cards.html" class="nav-link"> <i class="nav-icon bi bi-circle"></i>
-                                        <p>Cards</p>
+                                
+                            </ul>
+                        </li>
+                        <li class="nav-item"> <a href="#" class="nav-link"> <i class="bi bi-capsule-pill"></i>
+                                <p>
+                                   Medicine
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Add Category</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Category List</p>
+                                    </a> </li>
+                                
+                            </ul>
+                        </li>
+                        <li class="nav-item"> <a href="#" class="nav-link"><i class="bi bi-person-lines-fill"></i>
+                                <p>
+                                   Supplier
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Add Supplier</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Supplier List</p>
+                                    </a> </li>
+                             <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Supplier Ledger</p>
+                                    </a> </li>
+                                
+                            </ul>
+                        </li>
+                        <li class="nav-item"> <a href="#" class="nav-link"> <i class="bi bi-receipt-cutoff"></i>
+                                <p>
+                                   Invoice
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Add Invoice</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Invoice List</p>
+                                    </a> </li>
+                                
+                            </ul>
+                        </li>
+                        <li class="nav-item"> <a href="#" class="nav-link"> <i class="bi bi-backpack4-fill"></i>
+                                <p>
+                                   Return
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Invoice Return List</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Supplier Return List</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                    <p>Wastage Return List</p>
+                                    </a> </li>
+                                
+                            </ul>
+                        </li>
+
+                        <li class="nav-item"> <a href="#" class="nav-link"> <i class="bi bi-shop"></i>
+                                <p>
+                                   Stock
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Stock Report</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Stock Report (Batch Wise)</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                    <p>Available Stock</p>
+                                    </a> </li>
+                                
+                            </ul>
+                        </li>
+                        <li class="nav-item"> <a href="#" class="nav-link"><i class="bi bi-people-fill"></i>
+                                <p>
+                                   Customer
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Add Customer</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Customer List</p>
+                                    </a> </li>
+                              
+                                
+                            </ul>
+                        </li>
+
+                        <li class="nav-item"> <a href="#" class="nav-link"><i class="bi bi-book"></i>
+                                <p>
+                                   Report
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>User Report</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>User Wise sale Report</p>
+                                    </a> </li>
+                              <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Product Wise Sales Report</p>
+                                    </a> </li>
+                                    <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Category Wise Sales Report</p>
+                                    </a> </li> 
+                                    <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Purchase Report</p>
+                                    </a> </li>      
+                                
+                            </ul>
+                        </li>
+                        <li class="nav-item"> <a href="#" class="nav-link"><i class="bi bi-person-workspace"></i>
+                                <p>
+                                   Role
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item"> <a href="./widgets/small-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Add Role</p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Role List</p>
+                                    </a> </li>
+                                    <li class="nav-item"> <a href="./widgets/info-box.html" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Assign List</p>
                                     </a> </li>
                             </ul>
                         </li>
+                        <li class="nav-item"> <a href="#" class="nav-link"><i class="bi bi-gear"></i>
+                                <p>
+                                   Setting
+                                    <i class="nav-arrow bi bi-chevron-right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item"> <a href="#" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Add </p>
+                                    </a> </li>
+                                <li class="nav-item"> <a href="#" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>add</p>
+                                    </a> </li>
+                                    <li class="nav-item"> <a href="#" class="nav-link"><i class="bi bi-arrow-right-short ms-3"></i>
+                                        <p>Assign List</p>
+                                    </a> </li>
+                            </ul>
+                        </li>
+                        
+                        
+                        <li class="nav-item"> <a href="./generate/theme.html" class="nav-link"> <i class="nav-icon bi bi-palette"></i>
+                                <p>Theme Generate</p>
+                            </a> </li>
                       
                     </ul> <!--end::Sidebar Menu-->
                 </nav>
