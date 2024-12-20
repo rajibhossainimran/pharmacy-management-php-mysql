b8b42c903c36a2813ac911b074221e7ccb7bc1de
rajibhossainimran <rajibhossainimran@gmail.com>
2024-12-20 22:54:13 +0600

    dashboard customaize

--- dashboard.php
+++ dashboard.php
@@ -1,37 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tailwind CSS via CDN</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
-   <!-- header part start  -->
-<header>
+      <!-- header part start  -->
+      <?php 
+       include("./pages/common_pages/header.php");
+       ?>
+      <!-- header part end -->
 
-</header>
-   <!-- header part end  -->
 
-<!-- sidebar part start  -->
-<aside>
 
-</aside>
-<!-- sidebar part end  -->
+        <!--Sidebar part start-->
+        <?php 
+       include("./pages/common_pages/sidebar.php");
+       ?>
+        <!--Sidebar part end-->
+        
+      <!-- main part start  -->
+      <!-- main part end -->
 
-<!-- footer part start  -->
 
-<footer class="bg-teal-900 text-white py-6">
-  <div class="container mx-auto">
-    <!-- Bottom Text -->
-    <div class="text-center text-gray-200 mt-2">
-      © 2024 MyWebsite. All rights reserved.
-    </div>
-  </div>
-</footer>
-
-<!-- footer part end  -->
-
-</body>
-</html>
+        <!-- footer part start  -->
+       <?php 
+       include("./pages/common_pages/footer.php");
+       ?>
+       <!-- footer part end  -->
 
